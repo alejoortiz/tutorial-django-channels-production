4eb18209e3f7198024be1942a9a0c57801aa45c5
alejorti <alejorti@cisco.com>
2020-11-01 18:54:34 -0500

    HTTP and static files working
    HTTP and static files working

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,7 +1,7 @@
-"""app URL Configuration
+"""mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,10 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
-from hello_world import views
+from django.conf.urls import include
 
 urlpatterns = [
+    path('chat/', include('chat.urls')),
     path('admin/', admin.site.urls),
-    path('', views.index)
 ]
