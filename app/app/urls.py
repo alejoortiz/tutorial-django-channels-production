7d3e5b1f4b29d7b255cde99cc4a8ad4a1522ef5f
alejorti <alejorti@cisco.com>
2020-11-01 19:00:10 -0500

    app hello and app chat working
    app hello and app chat working

--- app/app/urls.py
+++ app/app/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from django.conf.urls import include
 
 urlpatterns = [
+    path('hello/', include('hello_world.urls')),
     path('chat/', include('chat.urls')),
     path('admin/', admin.site.urls),
 ]
