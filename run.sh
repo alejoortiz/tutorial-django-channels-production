05433050a5d6d252f99b105c298f8bc9ae41ae22
alejorti <alejorti@cisco.com>
2020-11-02 20:05:06 -0500

    nginx-gunicorn-daphne
    nginx with http and websockets in production final test done

--- run.sh
+++ run.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
-docker-compose down
-# stop containers
-docker stop $(docker ps -a | egrep 'django' | awk '{print $1}')
-# erase containers
-docker rm $(docker ps -a | egrep 'django' | awk '{print $1}')
-# remove wsgiserver images
-# docker rmi -f $(docker images | egrep 'wsgiserver' | awk '{print $3}')
-# remove asgiserver images
-# docker rmi -f $(docker images | egrep 'asgiserver' | awk '{print $3}')
-# remove proxy images
-# docker rmi -f $(docker images | egrep 'proxy' | awk '{print $3}')
 # start containers
-docker-compose up --build
+docker-compose up -d --build
